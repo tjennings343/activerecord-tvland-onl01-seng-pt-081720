4f038fe89055d5cd2d1e4c11fd46f01f081196a3
tjennings343 <tjennings343@gmail.com>
2020-11-05 22:56:50 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    name = "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    character = self.characters.collect do |t|
+        t.name
+    end
+    show = self.shows.collect do |s|
+        s.name
+    end
+    role = character.concat show
+    roles << role.join(" - ")
+    roles
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actor_full_name = []
+    actor_first_name = self.actors.collect do |f| 
+        f.first_name
+    end
+    actor_last_name = self.actors.collect do |l|
+        l.last_name
+    end
+    actor_name = actor_first_name + actor_last_name
+    actor_full_name << actor_name.join(" ")
+    actor_full_name 
+  end
 end
+
